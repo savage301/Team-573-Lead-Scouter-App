c425a27dbd3d0e8feafe4f896bbfca20c8fcc694
PIPIPIG233666 <pig.priv@gmail.com>
2022-09-02 19:47:18 -0400

    Move scanner view to a fragment
    - Click on the button to fire up the scanner

--- app/src/main/java/com/pppig236/scoutingappredo/MainActivity.kt
+++ app/src/main/java/com/pppig236/scoutingappredo/MainActivity.kt
@@ -8,30 +8,17 @@ import android.os.Environment
 import android.provider.Settings
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.budiyev.android.codescanner.*
 import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var codeScanner: CodeScanner
     private val permissionsRequestCode = 123
     private lateinit var managePermissions: ManagePermissions
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
 
-        codeScanner = CodeScanner(this, scannerView)
-        // Parameters (default values)
-        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
-        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
-        // ex. listOf(BarcodeFormat.QR_CODE)
-        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
-        codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
-        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
-        codeScanner.isFlashEnabled = false // Whether to enable flash or not
         // Initialize a list of required permissions to request runtime
         val list = listOf(
             Manifest.permission.CAMERA,
@@ -41,11 +28,17 @@ class MainActivity : AppCompatActivity() {
         // Initialize a new instance of ManagePermissions class
         managePermissions = ManagePermissions(this, list, permissionsRequestCode)
 
-        val button = findViewById<Button>(R.id.btnRequest)
-        // Button to check permissions states
-        showHide(button)
+        val buttonScanner = findViewById<Button>(R.id.scanner_view)
+        buttonScanner.setOnClickListener {
+            val fragment = ScannerFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit()
+        }
 
-        button.setOnClickListener {
+        val buttonPerm = findViewById<Button>(R.id.btnRequest)
+        // Button to check permissions states
+        showHide(buttonPerm)
+
+        buttonPerm.setOnClickListener {
             managePermissions.checkPermissions()
             if (Environment.isExternalStorageManager()) {
                 // create a new csv file
@@ -60,41 +53,6 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
-
-        // TO-DO: make a home page to display all the data!
-        // Callbacks
-        codeScanner.decodeCallback = DecodeCallback {
-            showHide(button)
-
-            runOnUiThread {
-                Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
-            }
-            appendCsv(
-                Environment.getExternalStorageDirectory().path + "/Download/data.csv",
-                "\n${it.text}"
-            )
-        }
-        codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
-            runOnUiThread {
-                Toast.makeText(
-                    this, "Camera initialization error: ${it.message}",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        }
-        scannerView.setOnClickListener {
-            codeScanner.startPreview()
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        codeScanner.startPreview()
-    }
-
-    override fun onPause() {
-        codeScanner.releaseResources()
-        super.onPause()
     }
 
     private fun showHide(view: View) {

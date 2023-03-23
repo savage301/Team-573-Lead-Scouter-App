e5ea49e9f9376507d6fa7730ec375c37350222dd
PIPIPIG233666 <pig.priv@gmail.com>
2023-03-22 22:20:19 -0400

    add mobility

--- app/src/main/java/com/pppig236/scoutingappredo/CSVOperations.kt
+++ app/src/main/java/com/pppig236/scoutingappredo/CSVOperations.kt
@@ -12,7 +12,7 @@ class CSVOperations {
 
         // create a new file with headers
         // Update this
-        file.writeText("Match Number,Alliance,Team Number,Auto High Cone,Auto Mid Cone,Auto Low Cone,Auto High Cube,Auto Mid Cube,Auto Low Cube,Auto Charge Station,Tele High Cone,Tele Mid Cone,Tele Low Cone,Tele High Cube,Tele Mid Cube,Tele Low Cube,Links,Tele Charge Station,Penalties,Robot Breakdown,Defense,Comment")
+        file.writeText("Match Number,Alliance,Team Number,Auto High Cone,Auto Mid Cone,Auto Low Cone,Auto High Cube,Auto Mid Cube,Auto Low Cube,Auto Charge Station,Mobility,Tele High Cone,Tele Mid Cone,Tele Low Cone,Tele High Cube,Tele Mid Cube,Tele Low Cube,Links,Tele Charge Station,Penalties,Robot Breakdown,Defense,Comment")
     }
 
     fun appendCsv(fileName: String, text: String) {
